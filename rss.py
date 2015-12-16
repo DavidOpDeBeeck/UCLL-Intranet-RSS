d6aec9c008e0ce317e97eb53b002253d9511b8ab
David Op de Beeck <davidopdebeeck@hotmail.com>
2015-12-16 17:46:01 +0100

    Added spaces behind HTML identifiers <p> and <br/> because Slack doesn't respect paragraphs or new lines.

--- rss.py
+++ rss.py
@@ -143,7 +143,7 @@ for page_element in enumerate(page_items_html[:-2]):  # Remove 'volgende' and 'l
                     for message_description_link in message_description_links:  # If link is relative add prefix
                         if message_description_link and message_description_link['href'][0] == '/':
                             message_description_link['href'] = link_prefix_url + message_description_link['href']
-                    channel_item_description.text += str(message_description_text).decode("utf8")
+                    channel_item_description.text += str(message_description_text).decode("utf8").replace("</p>", "</p> ").replace("<br/>", "<br/> ")
 
             if message_attachments:  # Only add attachments if found
                 channel_item_description.text += '<h3>Attachment</h3>'
